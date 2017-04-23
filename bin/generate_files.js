26e5c32d3853e14dfe88168d93b27fcb28eb0977
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2017-04-24 00:26:03 +0430

    All locales.

--- bin/generate_files.js
+++ bin/generate_files.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
+// Currencies
 const currenciesDir = path.resolve(__dirname + '/../currencies')
 const currenciesIndexFilePath = path.resolve(currenciesDir + '/index.json')
 
@@ -18,3 +19,22 @@ const currencies = currencyFiles.reduce((p, c) => {
 }, {})
 
 fs.writeFileSync(currenciesIndexFilePath, JSON.stringify(currencies, null, 2))
+
+// Locales
+const localesDir = path.resolve(__dirname + '/../locales')
+const localesIndexFilePath = path.resolve(localesDir + '/index.json')
+
+if (fs.existsSync(localesIndexFilePath)) {
+  fs.unlinkSync(localesIndexFilePath)
+}
+
+const localeFiles = fs.readdirSync(localesDir)
+const locales = localeFiles.reduce((p, c) => {
+  const fileContent = fs.readFileSync(path.resolve(localesDir + "/" + c))
+  const localeInfo = JSON.parse(fileContent)
+  p[c.replace('.json', '')] = localeInfo
+
+  return p
+}, {})
+
+fs.writeFileSync(localesIndexFilePath, JSON.stringify(locales, null, 2))

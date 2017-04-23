5b1e3094cf1349dbffb22917b55632fff2624274
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2017-04-23 22:34:51 +0430

    Separated the locale data.

--- bin/separate_currencies.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const currencies = require('../currencies')
-
-currencies.forEach(c => {
-  fs.writeFileSync(path.resolve(__dirname, "../currencies/" + c.code +".json"), JSON.stringify(c, null, 2))
-})

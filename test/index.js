d43a6fc8dc3186ef7e1c69eabee09a68b576bad4
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2017-04-23 23:19:17 +0430

    Additional unit tests to test symbol left, right, space and no space.

--- test/index.js
+++ test/index.js
@@ -3,11 +3,14 @@ should()
 
 import USD from '../currencies/USD'
 import EUR from '../currencies/EUR'
+import ARS from '../currencies/ARS'
+import ERN from '../currencies/ERN'
 import { format } from '../src'
 
 describe('format when', () => {
   context('no default is set', () => {
     context('only currency is passed', () => {
+      // Symbol left - no space
       context('for USD', () => {
         it('should return -$1.00 for -1', () => {
           const result = format(-1, USD)
@@ -45,6 +48,83 @@ describe('format when', () => {
         })
       })
 
+      // Symbol left - with space
+      context('for ARS', () => {
+        it('should return -$ 1,00 for -1', () => {
+          const result = format(-1, ARS)
+          result.should.equal('-$ 1,00')
+        })
+
+        it('should return $ 0,00 for 0', () => {
+          const result = format(0, ARS)
+          result.should.equal('$ 0,00')
+        })
+
+        it('should return $ 1,00 for 1', () => {
+          const result = format(1, ARS)
+          result.should.equal('$ 1,00')
+        })
+
+        it('should return $ 10,00 for 10', () => {
+          const result = format(10, ARS)
+          result.should.equal('$ 10,00')
+        })
+
+        it('should return $ 100,00 for 100', () => {
+          const result = format(100, ARS)
+          result.should.equal('$ 100,00')
+        })
+
+        it('should return $ 1.000,00 for 1000', () => {
+          const result = format(1000, ARS)
+          result.should.equal('$ 1.000,00')
+        })
+
+        it('should return $ 10.000,00 for 10000', () => {
+          const result = format(10000, ARS)
+          result.should.equal('$ 10.000,00')
+        })
+      })
+
+      // Symbol righr - no space
+      context('for ERN', () => {
+        it('should return -1.00Nfk for -1', () => {
+          const result = format(-1, ERN)
+          result.should.equal('-1.00Nfk')
+        })
+
+        it('should return 0.00Nfk for 0', () => {
+          const result = format(0, ERN)
+          result.should.equal('0.00Nfk')
+        })
+
+        it('should return 1.00Nfk for 1', () => {
+          const result = format(1, ERN)
+          result.should.equal('1.00Nfk')
+        })
+
+        it('should return 10.00Nfk for 10', () => {
+          const result = format(10, ERN)
+          result.should.equal('10.00Nfk')
+        })
+
+        it('should return 100.00Nfk for 100', () => {
+          const result = format(100, ERN)
+          result.should.equal('100.00Nfk')
+        })
+
+        it('should return 1,000.00Nfk for 1000', () => {
+          const result = format(1000, ERN)
+          result.should.equal('1,000.00Nfk')
+        })
+
+        it('should return 10,000.00Nfk for 10000', () => {
+          const result = format(10000, ERN)
+          result.should.equal('10,000.00Nfk')
+        })
+      })
+
+      // Symbol right - with space
       context('for EUR', () => {
         it('should return -1,00 € for -1', () => {
           const result = format(-1, EUR)

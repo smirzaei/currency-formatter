b7522ebc97a45c9ac905ce33198e0e1be3e97f26
Scott Sims <ssims@epoch.com>
2020-03-30 14:30:53 -0700

    Cleaning up style

--- currencies.js
+++ currencies.js
@@ -1,8 +1,8 @@
-const formatter = require('./index')
+const formatter = require('./index');
 
 /**
  * This module exists purely for backwards compatibility reasons. Historically, the currencies
  * would be stored in an array available through the default export from this exact file.
  * We've opted to store currencies in a JSON object now (currencies.json).
  */
-module.exports = formatter.currencies
+module.exports = formatter.currencies;
